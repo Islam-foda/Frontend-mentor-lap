6e23739b0c20aadb4d49b5b7d050e61f238dcf83
islam-foda <Eslam.foda@hotmail.com>
2024-11-24 12:52:33 +0200

    build project

--- fylo-landing-page-with-two-column-layout-master/vite.config.js
+++ fylo-landing-page-with-two-column-layout-master/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: 'Frontend-Mentor/fylo-landing-page-with-two-column-layout-master/',
   plugins: [react()],
 })
